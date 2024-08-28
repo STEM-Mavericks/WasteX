c30568fa7b5a669647cef5ba141e2ce8cdf4c1c6
STEM Mavericks <stemavericks.team@hotmail.com>
2024-08-28 20:03:04 +0530

    Update app.py

--- Codes/app.py
+++ Codes/app.py
@@ -1,22 +1,3 @@
-"""
-File: app.py (Automated-Waste-Segregation-System)
-Description: This file contains the main algorithm for the Automated Waste Segregation System.
-
-Copyright 2024 STEM Mavericks
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
