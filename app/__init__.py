5a9e2a0d4c897886cc0c2f0be44f3fb06b289eb6
agm024 <161752478+agm024@users.noreply.github.com>
2024-08-31 16:59:44 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config.from_object('config.config')
+
+db = SQLAlchemy(app)
+
+from .routes import main_bp
+app.register_blueprint(main_bp)
+
+def init_db():
+    db.create_all()
+
