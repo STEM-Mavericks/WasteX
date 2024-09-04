1c4500bd241a496ff0ecb13a24223986fd1b5f2c
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-04 20:18:37 +0000

    Update

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
+app = Flask(__name__)
+
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 
 def create_app():
-    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
     
