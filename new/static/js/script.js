78dda234d8764e6bb2ee2fb795c32c3c6954eb6f
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-05 10:57:30 +0000

    Updating Whole Directory

--- new/static/js/script.js
+++
@@ -1,9 +0,0 @@
-// Add any custom JavaScript you need here
-function togglePasswordVisibility() {
-    const passwordField = document.getElementById('password');
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-    } else {
-        passwordField.type = 'password';
-    }
-}
