9b86fb8a6f9da661ea9aef7e04bd4a4cdcb0b3a0
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-13 09:28:19 +0000

    Auto Commit

--- myapp/autocommit.sh
+++ myapp/autocommit.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
 while true; do
-  # Stash any unstaged changes
   git stash -u
-  
-  # Pull the latest changes with rebase
   git pull --rebase
-  
-  # Apply stashed changes back
   git stash pop
-  
-  # Stage all changes
   git add .
-  
-  # Commit the changes
   git commit -m "Auto Commit"
-  
-  # Push the changes to the remote repository
   git push
-  
-  # Wait for 5 minutes before the next iteration
-  sleep 300
+  sleep 100
 done
