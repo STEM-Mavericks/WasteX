dad06c68cefeaced78924cb83acf12853b5be4fe
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-13 21:07:37 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, url_for, flash, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required, login_remembered
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -9,6 +9,7 @@ from itsdangerous import URLSafeTimedSerializer as Serializer
 from flask_mail import Message, Mail
 import os
 from datetime import datetime
+
 app = Flask(__name__)
 
 # Config.py merged in this app.py
@@ -42,7 +43,7 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def get_reset_token(self, expires_sec=3600):
         s = Serializer(app.config['SECRET_KEY'])
@@ -73,8 +74,7 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.created_at}')"
 
-    
-# FORMSS
+# FORMS
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -118,22 +118,22 @@ class ResetPasswordForm(FlaskForm):
 def home():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('dashboard'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password(user.password, form.password.data):
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             flash('Login Successful', 'success')
             return redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password', 'danger')
-        return render_template('login.html', form=form)
+    return render_template('login.html', form=form)
     
-@app.route('/register', methods=['GET','POST'])
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
@@ -163,7 +163,7 @@ def confirm_email(token):
     flash('Your email has been confirmed!', 'success')
     return redirect(url_for('login'))
 
-@app.route('/reset-password', methods=['GET','POST'])
+@app.route('/reset-password', methods=['GET', 'POST'])
 def reset_request():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
@@ -172,9 +172,61 @@ def reset_request():
         user = User.query.filter_by(email=form.email.data).first()
         if user:
             token = user.get_reset_token()
-            send_reset_token(user.email, token)
-        flash('An email has been sent with instruction to your inbox to reset the password', 'info')
+            send_reset_email(user.email, token)
+        flash('An email has been sent with instructions to reset your password', 'info')
         return redirect(url_for('login'))
     return render_template('reset_request.html', form=form)
 
-@app.route('/reset-password/<token>', methods=['GET'])
+@app.route('/reset-password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('Invalid or expired token', 'danger')
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', form=form)
+
+@app.route('/profile', methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        if form.password.data:
+            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+            current_user.password = hashed_password
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    return render_template('profile.html', form=form)
+
+def send_confirmation_email(to_email, token):
+    msg = Message('Confirm Your Email', sender='noreply@example.com', recipients=[to_email])
+    msg.body = f'''To confirm your account, visit the following link:
+{url_for('confirm_email', token=token, _external=True)}
+
+If you did not make this request, simply ignore this email.
+'''
+    mail.send(msg)
+
+def send_reset_email(to_email, token):
+    msg = Message('Password Reset Request', sender='noreply@example.com', recipients=[to_email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request, simply ignore this email.
+'''
+    mail.send(msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
