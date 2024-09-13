9c4f84b718e50378d036958e08b7c50641b10da3
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-13 09:26:35 +0000

    Auto Commit

--- myapp/autocommit.sh
+++ myapp/autocommit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true; do
+  git pull --rebase        # Pull the latest changes from the remote branch
   git add .
   git commit -m "Auto Commit"
   git push
   sleep 300
 done
-# Use this when starting to write any code
