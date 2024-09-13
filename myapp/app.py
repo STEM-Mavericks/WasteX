3b0964a7e158fd0a888b033a4bd61addb877f498
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-13 20:23:42 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,9 @@
 from flask import Flask, request, redirect, url_for, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-from flask_login import 
+from flask_login import LoginManager, UserMixin, login_user, login_remembered, current_user, logout_user, login_required
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from dotenv import load_dotenv
+import os
