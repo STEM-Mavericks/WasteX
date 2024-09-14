12e3ca6501872e25692a41078fa83a0d625962f0
STEM Mavericks <stemavericks.team@hotmail.com>
2024-09-14 20:43:17 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # Configurations
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///instance/site.db'
     MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
