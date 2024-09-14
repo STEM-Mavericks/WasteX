08f9a9c290849a4258419d714380cc884e15e44a
STEM Mavericks <stemavericks.team@hotmail.com>
2024-09-14 20:39:53 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -6,39 +6,39 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from itsdangerous import URLSafeTimedSerializer as Serializer
-from flask_mail import Message, Mail
+from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
 from datetime import datetime
 
+# Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
 
-# Config.py merged in this app.py
+# Configurations
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    MAIL_SERVER = 'smtp.hotmail.com'
+    MAIL_SERVER = 'smtp-mail.outlook.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
 
 app.config.from_object(Config)
 
-# __init__.py merged in app.py
+# Initialize extensions
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 mail = Mail(app)
+serializer = Serializer(app.config['SECRET_KEY'])
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
+# User model and token methods
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -49,35 +49,32 @@ class User(db.Model, UserMixin):
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
 
     def get_reset_token(self, expires_sec=3600):
-        s = Serializer(app.config['SECRET_KEY'])
-        return s.dumps({'user_id': self.id}, salt='password-reset-salt')
+        return serializer.dumps({'user_id': self.id}, salt='password-reset-salt')
     
     @staticmethod
     def verify_reset_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token, salt='password-reset-salt', max_age=3600)['user_id']
+            user_id = serializer.loads(token, salt='password-reset-salt', max_age=3600)['user_id']
         except:
             return None
         return User.query.get(user_id)
     
     def get_confirmation_token(self, expires_sec=3600):
-        s = Serializer(app.config['SECRET_KEY'])
-        return s.dumps({'user_id': self.id}, salt='confirm-email-salt')
+        return serializer.dumps({'user_id': self.id}, salt='confirm-email-salt')
     
     @staticmethod
     def verify_confirmation_token(token):
-        s = Serializer(app.config['SECRET_KEY'])
         try:
-            user_id = s.loads(token, salt='confirm-email-salt', max_age=3600)['user_id']
+            user_id = serializer.loads(token, salt='confirm-email-salt', max_age=3600)['user_id']
         except:
             return None
         return User.query.get(user_id)
-    
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.created_at}')"
 
-# FORMS
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Forms
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -101,12 +98,6 @@ class LoginForm(FlaskForm):
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
 
-class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[Length(min=8)])
-    submit = SubmitField('Update')
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
@@ -116,106 +107,10 @@ class ResetPasswordForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
 
-# Routes
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user and bcrypt.check_password_hash(user.password, form.password.data):
-            login_user(user, remember=form.remember.data)
-            flash('Login Successful', 'success')
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Invalid username or password', 'danger')
-    return render_template('login.html', form=form)
-    
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(user)
-        db.session.commit()
-
-        # Send email confirmation
-        token = user.get_confirmation_token()
-        send_confirmation_email(user.email, token)
-
-        flash('Your account has been created! Please check your email to confirm your account.', 'success')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-    
-@app.route('/confirm_email/<token>')
-def confirm_email(token):
-    user = User.verify_confirmation_token(token)
-    if user is None:
-        flash('Invalid or expired token', 'danger')
-        return redirect(url_for('index'))
-    user.confirmed = True
-    db.session.commit()
-    flash('Your email has been confirmed!', 'success')
-    return redirect(url_for('login'))
-
-@app.route('/reset-password', methods=['GET', 'POST'])
-def reset_request():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    form = RequestResetForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(email=form.email.data).first()
-        if user:
-            token = user.get_reset_token()
-            send_reset_email(user.email, token)
-        flash('An email has been sent with instructions to reset your password', 'info')
-        return redirect(url_for('login'))
-    return render_template('reset_request.html', form=form)
-
-@app.route('/reset-password/<token>', methods=['GET', 'POST'])
-def reset_token(token):
-    user = User.verify_reset_token(token)
-    if user is None:
-        flash('Invalid or expired token', 'danger')
-        return redirect(url_for('index'))
-    form = ResetPasswordForm()
-    if form.validate_on_submit():
-        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user.password = hashed_password
-        db.session.commit()
-        flash('Your password has been updated! You are now able to log in', 'success')
-        return redirect(url_for('login'))
-    return render_template('reset_token.html', form=form)
-
-@app.route('/profile', methods=['GET', 'POST'])
-@login_required
-def profile():
-    form = UpdateAccountForm()
-    if form.validate_on_submit():
-        current_user.username = form.username.data
-        current_user.email = form.email.data
-        if form.password.data:
-            hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-            current_user.password = hashed_password
-        db.session.commit()
-        flash('Your account has been updated!', 'success')
-        return redirect(url_for('profile'))
-    elif request.method == 'GET':
-        form.username.data = current_user.username
-        form.email.data = current_user.email
-    return render_template('profile.html', form=form)
-
-def send_confirmation_email(to_email, token):
-    msg = Message('Confirm Your Email', sender='noreply@example.com', recipients=[to_email])
+# Email functions
+def send_confirmation_email(to_email):
+    token = serializer.dumps(to_email, salt='email-confirm')
+    msg = Message('Confirm Your Email', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[to_email])
     msg.body = f'''To confirm your account, visit the following link:
 {url_for('confirm_email', token=token, _external=True)}
 
@@ -223,14 +118,92 @@ If you did not make this request, simply ignore this email.
 '''
     mail.send(msg)
 
-def send_reset_email(to_email, token):
-    msg = Message('Password Reset Request', sender='noreply@example.com', recipients=[to_email])
+def send_reset_email(to_email):
+    token = serializer.dumps(to_email, salt='password-reset-salt')
+    msg = Message('Password Reset Request', sender=app.config['MAIL_DEFAULT_SENDER'], recipients=[to_email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
 
 If you did not make this request, simply ignore this email.
 '''
     mail.send(msg)
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            flash('Login Successful', 'success')
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+
+        send_confirmation_email(user.email)
+        flash('Your account has been created! Please check your email to confirm your account.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    email = serializer.loads(token, salt='email-confirm')
+    user = User.query.filter_by(email=email).first()
+    if user:
+        user.confirmed = True
+        db.session.commit()
+        flash('Your email has been confirmed!', 'success')
+        return redirect(url_for('login'))
+    else:
+        flash('Invalid or expired token', 'danger')
+        return redirect(url_for('index'))
+
+@app.route('/reset-password', methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user:
+            send_reset_email(user.email)
+        flash('An email has been sent with instructions to reset your password', 'info')
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', form=form)
+
+@app.route('/reset-password/<token>', methods=['GET', 'POST'])
+def reset_token(token):
+    user = User.verify_reset_token(token)
+    if not user:
+        flash('Invalid or expired token', 'danger')
+        return redirect(url_for('index'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        flash('Your password has been updated! You can now log in.', 'success')
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', form=form)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
