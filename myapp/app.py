8dcfe9a6219ca10c15e70677b9315801978c60aa
agm024 <161752478+agm024@users.noreply.github.com>
2024-09-14 06:44:53 +0000

    Auto Commit

--- myapp/app.py
+++ myapp/app.py
@@ -232,4 +232,4 @@ If you did not make this request, simply ignore this email.
     mail.send(msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
